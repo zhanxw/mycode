fa09e6c7109caf223fbb20b71805815f84286a57
zhanxw <zhanxw@gmail.com>
2011-06-05 18:19:21 -0400

    add sizeof

--- omp_function/omp_test.c
+++ omp_function/omp_test.c
@@ -3,10 +3,21 @@
 #include <stdlib.h>
 
 int main() {
-    // omp_set_num_threads(12); that affectsomp_get_max_threads()
     printf("omp_get_num_threads() [default value] = %d\n", omp_get_num_threads());
     printf("omp_get_max_threads() = %d \n", omp_get_max_threads());
     printf("omp_get_num_procs() = %d\n", omp_get_num_procs());
+    printf("\n");
+
+    
+    omp_set_num_threads(2); // that affectsomp_get_max_threads()
+    #pragma omp parallel for ordered 
+    for (int i = 0; i < omp_get_max_threads(); i ++) {
+        printf("Thread %d of total %d thread\n", omp_get_thread_num(), omp_get_num_threads());
+    }
+    printf("omp_get_num_threads() = %d (Always one in the sequencial part)\n", omp_get_num_threads());
+
+
+    printf("\n");
     printf("omp_get_wtime() = %f\n", omp_get_wtime() );
     printf("omp_get_wtick() = %f\n", omp_get_wtick() );
     return 0;
---
+++ sizeof/Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdint.h>
+using std::cout;
+using std::endl;
+
+#define PRINT_SIZEOF(x) cout << "sizeof(" << #x << ") = " << sizeof(x) << endl;
+int main(int argc, char *argv[])
+{
+    PRINT_SIZEOF(char);
+    PRINT_SIZEOF(unsigned char);
+    PRINT_SIZEOF(signed char);
+    PRINT_SIZEOF(short int);
+    PRINT_SIZEOF(int);
+    PRINT_SIZEOF(long int);
+    PRINT_SIZEOF(long long);
+
+    PRINT_SIZEOF(unsigned int);
+    PRINT_SIZEOF(uint32_t );
+    PRINT_SIZEOF(uint64_t );
+
+    PRINT_SIZEOF(float);
+    PRINT_SIZEOF(double);
+
+
+    return 0;
+}
